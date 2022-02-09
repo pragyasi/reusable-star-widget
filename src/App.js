bcb519089632543dfd979add3f7ea1a32e26c46d
pragya singh <pragya.july@gmail.com>
2022-02-09 08:45:40 -0800

    minor refactor

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import "./styles.css";
 import StarWidget from "./StarWidget";
 
 export default function App() {
-  const handleRating = () => {
-    console.log("Hello");
-  };
   return (
     <div className="App">
-      <StarWidget
-        totalStars={5}
-        fillcolor="#ff1dce"
-        handleRating={handleRating}
-      />
+      <StarWidget totalStars={5} fillcolor="#ff1dce" />
     </div>
   );
 }
